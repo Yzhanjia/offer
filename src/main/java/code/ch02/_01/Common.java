c4ece953cb2353bd272b44e41bf4749f5e146591
Yzhanjia <37543890+Yzhanjia@users.noreply.github.com>
2018-08-15 23:55:46 +0800

    2018.8.15

--- src/main/java/code/ch02/_01/Common.java
+++ src/main/java/code/ch02/_01/Common.java
@@ -2,16 +2,21 @@ package code.ch02._01;
 
 import code.ch02.Node;
 
+/**
+ * 打印两个有序链表的公共部分
+ */
+
 public class Common {
 
     public void printCommonPart(Node head1, Node head2) {
         System.out.println("Common part: ");
         while (head1 != null && head2 != null) {
+            //移动较小链表
             if (head1.value < head2.value) {
                 head1 = head1.next;
             } else if (head1.value > head2.value) {
                 head2 = head2.next;
-            } else {
+            } else {//值相等的部分
                 System.out.println(head1.value + " ");
                 head1 = head1.next;
                 head2 = head2.next;
--- src/main/java/code/ch02/_02/RemoveLastKth.java
+++ src/main/java/code/ch02/_02/RemoveLastKth.java
@@ -4,6 +4,9 @@ package code.ch02._02;
 import code.ch02.DoubleNode;
 import code.ch02.Node;
 
+/**
+ * 在单链表和双链表中删除倒数第k个节点
+ */
 public class RemoveLastKth {
     public Node removeLastKthNode(Node head, int lastKth) {
         if (head == null || lastKth < 1) {
@@ -14,7 +17,7 @@ public class RemoveLastKth {
             lastKth--;
             cur = cur.next;
         }
-        if (lastKth == 0) {
+        if (lastKth == 0) {//删除头结点
             head = head.next;
         }
 //        if (lastKth > 0) {
@@ -22,11 +25,12 @@ public class RemoveLastKth {
 //        }
         if (lastKth < 0) {
             cur = head;
-            while (++lastKth != 0) {
+            while (++lastKth != 0) {//当lastKth为0时，找到要删除节点的前一个节点
                 cur = cur.next;
             }
             cur.next = cur.next.next;
         }
+        //k大于链表的节点数，不需调整直接返回头结点
         return head;
     }
 
@@ -41,7 +45,7 @@ public class RemoveLastKth {
         }
         if (lastKth == 0) {
             head = head.next;
-            head.last = null;
+            head.last = null;//多一步将指向上一节点设为NULL
         }
         if (lastKth < 0) {
             cur = head;
@@ -51,7 +55,7 @@ public class RemoveLastKth {
             DoubleNode newNext = cur.next.next;
             cur.next = newNext;
             if (newNext != null) {
-                newNext.last = cur;
+                newNext.last = cur;//多一步
             }
         }
         return head;
